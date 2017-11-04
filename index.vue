15e068bdcbfdc134498da0f744237c8f63a2387b
yangni007 <1650279773@qq.com>
2017-11-04 23:09:23 +0800

    swiper component

---
+++ index.vue
@@ -0,0 +1,65 @@
+<template>
+ 
+  <div>
+      <mySwiper width='100' height='150' :dataList='List'>
+          <div slot-scope='props' class='text-center'>
+            <p >{{props["text"].name}}</p>
+            <p class="font-fade">{{props["text"].inmovie}}</p>
+          </div>
+      </mySwiper>
+  </div>
+  </transition>
+
+<script>
+
+import mySwiper from './mySwiper'
+export default {
+  data () {
+    return {
+      List: [
+      {
+        src: 'https://static.vux.li/demo/3.jpg',
+        content: {
+          name: '张某某',
+          inmovie: '导演'
+        }
+        
+      },
+      {
+        src: 'https://static.vux.li/demo/3.jpg',
+        content: {
+          name: '赵丽颖',
+          inmovie:'兰芳'
+        }
+      },
+      {
+        src: 'https://static.vux.li/demo/3.jpg',
+        content: {
+          name: '郭富城',
+          inmovie:'林翔'
+        }
+      },
+      {
+        src: 'https://static.vux.li/demo/3.jpg',
+        content: {
+          name: '张翰',
+          inmovie:'梁栋'
+        }
+        
+      },
+        ]
+    }
+  },
+ mounted(){
+  
+ }
+}
+</script>
+<style lang='less' scoped>
+ .text-center {
+  text-align: center ;
+}
+.font-fade {
+  color: #999 ;
+}
+</style>
---
+++ mySwiper.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div  class='box' >
+      <ul :style='ul_width'>
+          <li :style='gap_li' v-for='(item, index) in List' :key='index'>
+            <img :src='item.src'/>
+            <slot :text='item.content'></slot>
+          </li>
+      </ul>
+  </div>
+
+</template>
+<script>
+export default {
+  data () {
+    return {
+      List : this.dataList,
+      default_gap: 10,
+      default_width: 30,
+      default_height:50,
+    }
+  },
+  props: ['gap','width', 'dataList', 'height'],
+  computed: {
+    gap_li(){
+      var str= 'width:'+(this.width||this.default_width)+'px;'
+                +'height:'+( this.height|| this.default_height)
+                +'px;margin-right:'+(this.gap||this.default_gap)+'px';
+      return str;
+    },
+    
+    ul_width(){
+
+      var str = 'width:'+this.List.length*(this.width+(this.gap||this.default_gap))+'px;';
+      return str;
+    }
+  },
+ mounted(){
+ 
+ }
+}
+</script>
+<style lang='less' scoped>
+.box{
+  overflow: auto;
+
+  ul{
+    display: inline-block;
+ 
+    list-style: none;
+
+    li{
+      float: left;
+      img{
+        width:100%;
+        height:70%;
+      }
+    }
+  }
+}
+</style>
